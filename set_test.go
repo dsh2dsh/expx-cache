4ceaf653991bee462f1df9e9ea0f9111e598b03f
Denis Shaposhnikov <993498+dsh2dsh@users.noreply.github.com>
2023-10-06 17:37:31 +0200

    Remove TestSet_canBeUsedWithIncr(),
    because we are not supporting serialized value can be used with raw redis
    commands.

--- set_test.go
+++ set_test.go
@@ -13,23 +13,6 @@ import (
 	redisMocks "github.com/dsh2dsh/expx-cache/mocks/redis"
 )
 
-func (self *CacheTestSuite) TestSet_canBeUsedWithIncr() {
-	if self.rdb == nil {
-		self.T().Skip("requires Redis connection")
-	}
-
-	ctx := context.Background()
-	value := "123"
-	self.Require().NoError(self.cache.Set(&Item{
-		Ctx:   ctx,
-		Key:   testKey,
-		Value: value,
-	}))
-
-	n := valueNoError[int64](self.T())(self.rdb.Incr(ctx, testKey).Result())
-	self.Equal(int64(124), n)
-}
-
 func TestCache_Set_Marshall_error(t *testing.T) {
 	localCache := cacheMocks.NewMockLocalCache(t)
 	cache := New().WithLocalCache(localCache).
